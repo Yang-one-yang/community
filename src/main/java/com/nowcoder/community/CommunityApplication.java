27cf91d7d45fbf98b3ec8cd4c3d88000919b6708
yangdong <19817861926@163.com>
2020-09-18 18:20:22 +0800

    20/09/18

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -2,7 +2,7 @@ package com.nowcoder.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//5b752c0be64b5eb5
 @SpringBootApplication
 public class CommunityApplication {
 
--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,5 @@
+var CINTEXT_PATH = "/community";
+
 window.alert = function(message) {
 	if(!$(".alert-box").length) {
 		$("body").append(
